284ab158046628cf8621b22ceb07118e84bccb0d
Nabir14 <mushfiquefarhannabir@gmail.com>
2024-10-14 23:14:23 +0600

    v0.4 - new keymaps and new method for setting icon

--- src/arcge.hpp
+++ src/arcge.hpp
@@ -1,3 +1,12 @@
+/*
+
+[      ArcGE     ]
+[Author: Nabir14 ]
+[Version: 0.4    ]
+[License: GPLv3  ]
+
+*/
+
 #pragma once
 
 #include <SDL2/SDL.h>
@@ -17,16 +26,50 @@
 #define ARCK_DOWN 11
 #define ARCK_LEFT 12
 #define ARCK_RIGHT 13
+#define ARCK_A 14
+#define ARCK_B 15
+#define ARCK_C 16
+#define ARCK_D 17
+#define ARCK_E 18
+#define ARCK_F 19
+#define ARCK_G 20
+#define ARCK_H 21
+#define ARCK_I 22
+#define ARCK_J 23
+#define ARCK_K 24
+#define ARCK_L 25
+#define ARCK_M 26
+#define ARCK_N 27
+#define ARCK_O 28
+#define ARCK_P 29
+#define ARCK_Q 30
+#define ARCK_R 31
+#define ARCK_S 32
+#define ARCK_T 33
+#define ARCK_U 34
+#define ARCK_V 35
+#define ARCK_W 36
+#define ARCK_X 37
+#define ARCK_Y 38
+#define ARCK_Z 39
 
 class ArcGE{
 	public:
 		SDL_Window* ArcGEWindow;
 		SDL_Renderer* ArcGERenderer;
 		SDL_Event ArcGEEvent;
+		int WINDOW_WIDTH = 0;
+		int WINDOW_HEIGHT = 0;
 	void init(const char* windowTitle, int windowWidth, int windowHeight){
 		SDL_Init(SDL_INIT_VIDEO);
 		ArcGEWindow = SDL_CreateWindow(windowTitle, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
 		ArcGERenderer = SDL_CreateRenderer(ArcGEWindow, -1, SDL_RENDERER_ACCELERATED);
+		this->WINDOW_WIDTH = windowWidth;
+		this->WINDOW_HEIGHT = windowHeight;
+	}
+	void setProgramIcon(const char* path){
+		SDL_Surface* icon = SDL_LoadBMP(path);
+		SDL_SetWindowIcon(ArcGEWindow, icon);
 	}
 };
 
@@ -81,6 +124,84 @@ class Scene{
 				break;
 			case SDLK_RIGHT:
 				return 13;
+				break;
+			case SDLK_a:
+				return 14;
+				break;
+			case SDLK_b:
+				return 15;
+				break;
+			case SDLK_c:
+				return 16;
+				break;
+			case SDLK_d:
+				return 17;
+				break;
+			case SDLK_e:
+				return 18;
+				break;
+			case SDLK_f:
+				return 19;
+				break;
+			case SDLK_g:
+				return 20;
+				break;
+			case SDLK_h:
+				return 21;
+				break;
+			case SDLK_i:
+				return 22;
+				break;
+			case SDLK_j:
+				return 23;
+				break;
+			case SDLK_k:
+				return 24;
+				break;
+			case SDLK_l:
+				return 25;
+				break;
+			case SDLK_m:
+				return 26;
+				break;
+			case SDLK_n:
+				return 27;
+				break;
+			case SDLK_o:
+				return 28;
+				break;
+			case SDLK_p:
+				return 29;
+				break;
+			case SDLK_q:
+				return 30;
+				break;
+			case SDLK_r:
+				return 31;
+				break;
+			case SDLK_s:
+				return 32;
+				break;
+			case SDLK_t:
+				return 33;
+				break;
+			case SDLK_u:
+				return 34;
+				break;
+			case SDLK_v:
+				return 35;
+				break;
+			case SDLK_w:
+				return 36;
+				break;
+			case SDLK_x:
+				return 37;
+				break;
+			case SDLK_y:
+				return 38;
+				break;
+			case SDLK_z:
+				return 39;
 				break;
 			}
                 }
---
+++ src/tests/test4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "../arcge.hpp"
+using namespace std;
+
+int main(){
+	ArcGE age;
+	age.init("ArcGE Window With Icon Example", 640, 480);
+	age.setProgramIcon("images/arcge-icon.bmp");
+	Scene myScene(&age);
+	bool run = true;
+	while(run){
+		switch(myScene.pollEvent()){
+			case ARCK_QUIT:
+				run = false;
+				break;
+			case ARCK_1:
+				myScene.clear(255, 0, 0, 255);
+				break;
+			case ARCK_2:
+				myScene.clear(0 ,255, 0, 255);
+				break;
+			case ARCK_3:
+				myScene.clear(0, 0, 255, 255);
+				break;
+		}
+		myScene.render();
+	}
+	myScene.freeMemory();
+	return 0;
+}
